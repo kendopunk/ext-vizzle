a2edc380bd0972a384ff05de4153b91aa5fbc60d
Mark Fehrenbacher <fehrenbacherm@gao.gov>
2014-01-22 10:04:25 -0500

    Separated US geo map into util file

---
+++ app/util/d3/geo/Us.js
@@ -0,0 +1,106 @@
+/**
+ * @class
+ * @author Mark Fehrenbacher (kendopunk@hotmail.com)
+ * @memberOf App.util.d3.geo
+ * @description Base US map class
+ */
+Ext.define('App.util.d3.geo.Us', {
+	extend: 'Object',
+	
+	requires: [
+		'App.util.MessageBus'
+	],
+	
+	svg: null,
+	canvasWidth: 500,
+	canvasHeight: 300,
+	projection: null,
+	path: null,
+	fill: '#FFCC33',
+	mouseOverFill: '#BADCDC',
+	stroke: '#333333',
+	strokeWidth: 0.5,
+	mapScale: 1000,
+	
+	constructor: function(config) {
+		var me = this;
+		
+		me.eventRelay = Ext.create('App.util.MessageBus');
+		
+		Ext.apply(me, config);
+	},
+	
+	/**
+ 	 * @function
+ 	 * @description Draw the map
+ 	 */
+	draw: function() {
+		var me = this;
+		
+		// define map projection
+		me.projection = d3.geo.albersUsa()
+			.translate([
+				me.canvasWidth/2,
+				me.canvasHeight/2
+			])
+			.scale([me.mapScale]);
+			
+		// define path generator
+		me.path = d3.geo.path()
+			.projection(me.projection);
+			
+		// local scope
+		var svg = me.svg,
+			path = me.path,
+			stroke = me.stroke,
+			fill = me.fill,
+			strokeWidth = me.strokeWidth,
+			mouseOverFill = me.mouseOverFill;
+			
+		// load in JSON data
+		d3.json('data/geo_us.json', function(json) {
+		
+			// Bind data and create one path per GeoJSON feature
+			svg.selectAll('path')
+				.data(json.features)
+				.enter()
+				.append('path')
+				.attr('d', path)
+				.style('fill', fill)
+				.style('stroke', stroke)
+				.style('stroke-width', strokeWidth)
+				.on('mouseover', function(d) {
+				
+					d3.select(this)
+						.style('fill', function(d) {
+							return mouseOverFill;
+						});
+				})
+				.on('mouseout', function(d) {
+					
+					d3.select(this)
+						.style('fill', fill);
+				});
+		});
+	},
+	
+	/**
+ 	 *
+ 	 *
+ 	 * SETTERS
+ 	 *
+ 	 *
+ 	 */
+ 	 
+ 	setSvg: function(s) {
+	 	var me = this;
+	 	me.svg = s;
+	},
+	
+	setCanvasDimensions: function(w, h) {
+		var me = this;
+		
+		me.canvasWidth = w;
+		me.canvasHeight = h;
+	}
+});
--- app/view/d3/geo/basic/MainPanel.js
+++ app/view/d3/geo/basic/MainPanel.js
@@ -11,143 +11,74 @@ Ext.define('App.view.d3.geo.basic.MainPanel', {
 	closable: true,
 	
 	requires: [
+		'App.util.d3.geo.Us',
 		'App.util.MessageBus'
 	],
 	
 	initComponent: function() {
 		var me = this;
 		
-		me.eventRelay = Ext.create('App.util.MessageBus')
+		/**
+ 		 * @properties
+ 		 * @description SVG properties
+ 		 */
+ 		me.svgInitialized = false,
+ 			me.graphData = [],
+ 			me.canvasWidth,
+ 			me.canvasHeight,
+ 			me.svg,
+ 			me.eventRelay = Ext.create('App.util.MessageBus'),
+ 			me.btnHighlightCss = 'btn-highlight-khaki',
+ 			me.width = 
+ 				parseInt((Ext.getBody().getViewSize().width - App.util.Global.westPanelWidth) * .95),
+ 			me.height = 
+	 			parseInt(Ext.getBody().getViewSize().height - App.util.Global.titlePanelHeight),
+	 		me.map = Ext.create('App.util.d3.geo.Us');
 		
-		// chart description for info panel
+		/**
+ 		 * @property
+ 		 * @description Chart description
+ 		 */
 		me.chartDescription = '<b>Basic Geo</b>';
-			
-		// on activate, publish update to the "Info" panel
+		
+		/**
+ 		 * @listener
+ 		 * @description On activate, publish update to the "info" panel
+ 		 */
 		me.on('activate', function() {
 			me.eventRelay.publish('infoPanelUpdate', me.chartDescription);
 		}, me);
 		
-		me.on('afterrender', me.draw, me);
-		
-		/*Ext.EventManager.onWindowResize(function(w, h) {
-			console.log(w + ' x ' + h);
-			me.setWidth(400);
-			me.setHeight(400);
-		}, me);*/
-		
-		/*me.on('resize', function(panel, w, h, eOpts) {
-			console.log('panel resized');
-		
-			me.svg.attr('width', 400)
-				.attr('height', 400);
-		
-		
-		
-		
-		});*/
-		
+		/**
+ 		 * @listener
+ 		 * @description After rendering, initialize the map and execute
+ 		 * other drawing functionality
+ 		 */
+ 		me.on('afterrender', me.initCanvas, me);
 		me.callParent(arguments);
 	},
 	
-	draw: function(panel) {
-		
-		var me = this;
-		
-		me.width = parseInt((Ext.getBody().getViewSize().width - App.util.Global.westPanelWidth) * .95);
-		me.height = parseInt(Ext.getBody().getViewSize().height - App.util.Global.titlePanelHeight);
-		
+	/**
+ 	 * @function
+ 	 * @description Initialize the canvas, map, etc.
+ 	 */
+ 	initCanvas: function(panel) {
+	 	var me = this;
+	 	
 		me.canvasWidth = parseInt(me.width * .95);
 		me.canvasHeight = parseInt(me.height * .95);
 		me.panelId = '#' + me.body.id;
-	 	
-	 	// init svg
-	 	me.svg = d3.select(me.panelId)
-	 		.append('svg')
-	 		.attr('width', me.canvasWidth)
-	 		.attr('height', me.canvasHeight)
-	 		.attr('preserveAspectRatio', 'none');
-	 		//.attr('viewBox', '0,0,300,300');
-	 	
-	 	// define map projection
-	 	var projection = d3.geo.albersUsa()
-		 	.translate([
-			 	me.canvasWidth/2,
-			 	me.canvasHeight/2
-			])
-			.scale([750]);
-
-		// define path generator
-		var path = d3.geo.path()
-			.projection(projection);
-			
-		var svg = me.svg;
-			
-
-		// load in JSON data
-		d3.json('data/geo_us.json', function(json) {
 		
-			// Bind data and create one path per GeoJSON feature
-			svg.selectAll('path')
-				.data(json.features)
-				.enter()
-				.append('path')
-				.attr('d', path)
-				.style('fill', '#FFCC33')
-				.style('stroke', 'black')
-				.style('stroke-width', 0.5)
-				/*.on('mouseover', function(d) {
-					svg.attr('viewBox', '0,0,300,300');
-				})
-				.on('mouseout', function(d) {
-					svg.attr('viewBox', '0, 0, ' + me.canvasWidth + ',' + me.canvasHeight);
-				});*/
-				.on('mouseover', function(d) {
-				
-					d3.select(this)
-						.style('opacity', .6)
-						.style('fill', function(d) {
-							if(d.properties.name == 'Ohio') {
-								return '#000000';
-							}
-							return '#CC3300';
-						
-						});
-				
-					//console.log(Ext.encode(a));
-				})
-				.on('mouseout', function(d) {
-					d3.select(this)
-						.style('opacity', 1)
-						.style('fill', '#FFCC33');
-				});
-			
-			// dayton
-			var long = 39.759444;
-			var lat = -84.191667;
-			var coordinates = projection([lat, long]);
-			
-			// try a circle
-			svg.selectAll('circle')
-				.data([1])
-				.enter()
-				.append('circle')
-				.attr('cx', coordinates[0])
-				.attr('cy', coordinates[1])
-				.attr('r', 5)
-				.style('fill', '#990066');
-				
-			/*Ext.each(json.features, function(item, index) {
-				console.log(item.properties.name);
-				console.log(index);
-				console.log('-------');
-			});*/
-				
-			/*Ext.each(path, function(item, index, all) {
-				console.debug(path(index));
-			});*/
-			
-			
-			
-		});
+		// init SVG
+		me.svg = d3.select(me.panelId)
+			.append('svg')
+			.attr('width', me.canvasWidth)
+			.attr('height', me.canvasHeight);
+		me.svgInitialized = true;
+		
+		// set map properties
+		me.map.setSvg(me.svg);
+		me.map.setCanvasDimensions(me.canvasWidth, me.canvasHeight);
+		me.map.draw();
 	}
 });
