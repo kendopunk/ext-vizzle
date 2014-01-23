fdf96147664794b7a1243fe270993c9fd0938d82
Mark Fehrenbacher <fehrenbacherm@gao.gov>
2014-01-23 16:53:49 -0500

    Filtering F5 data and docked items

--- app/view/d3/geo/basic/MainPanel.js
+++ app/view/d3/geo/basic/MainPanel.js
@@ -28,6 +28,8 @@ Ext.define('App.view.d3.geo.basic.MainPanel', {
  			me.canvasWidth,
  			me.canvasHeight,
  			me.svg,
+ 			me.gTitle,
+ 			me.gCircle,
  			me.eventRelay = Ext.create('App.util.MessageBus'),
  			me.btnHighlightCss = 'btn-highlight-khaki',
  			me.width = 
@@ -35,6 +37,7 @@ Ext.define('App.view.d3.geo.basic.MainPanel', {
  			me.height = 
 	 			parseInt(Ext.getBody().getViewSize().height - App.util.Global.titlePanelHeight),
 	 		me.usMapRenderedEvent = 'geoBasicRendered',
+	 		me.yearFilter = ['195'],
 	 		me.usMap = Ext.create('App.util.d3.geo.Us', {
 		 		fill: '#ECEECE',
 		 		mouseOverFill: '#CCCC99',
@@ -46,6 +49,101 @@ Ext.define('App.view.d3.geo.basic.MainPanel', {
  		 * @description Chart description
  		 */
 		me.chartDescription = '<b>Basic Geo</b>';
+		
+		/**
+ 		 * @property
+ 		 * @description Search button
+ 		 */
+ 		me.searchButton = Ext.create('Ext.button.Button', {
+	 		iconCls: 'icon-magnify',
+	 		tooltip: 'Search',
+	 		text: 'Search'
+	 	});
+		
+		/**
+ 	 	 * @property
+ 	 	 * @description Docked items
+ 	 	 */
+ 	 	 /*,
+		 	 	listeners: {
+			 	 	change: function(field) {
+				 	 	console.log(field.checked);
+				 	 	console.debug(field.inputValue);
+				 	}
+				 }*/
+ 	 	me.dockedItems = [{
+	 	 	xtype: 'toolbar',
+	 	 	dock: 'top',
+	 	 	items: [{
+		 	 	xtype: 'tbspacer',
+		 	 	width: 10
+		 	}, {
+		 	 	xtype: 'checkboxfield',
+		 	 	boxLabel: '1950-59',
+		 	 	name: 'years',
+		 	 	inputValue: '195',
+		 	 	checked: true
+		 	}, {
+			 	xtype: 'tbspacer',
+			 	width: 7
+			}, {
+		 	 	xtype: 'checkboxfield',
+		 	 	boxLabel: '1960-69',
+		 	 	name: 'years',
+		 	 	inputValue: '196',
+		 	 	checked: true
+		 	}, {
+			 	xtype: 'tbspacer',
+			 	width: 7
+			}, {
+		 	 	xtype: 'checkboxfield',
+		 	 	boxLabel: '1970-79',
+		 	 	name: 'years',
+		 	 	inputValue: '197',
+		 	 	checked: true
+		 	}, {
+			 	xtype: 'tbspacer',
+			 	width: 7
+			}, {
+		 	 	xtype: 'checkboxfield',
+		 	 	boxLabel: '1980-89',
+		 	 	name: 'years',
+		 	 	inputValue: '198',
+		 	 	checked: true
+		 	}, {
+			 	xtype: 'tbspacer',
+			 	width: 7
+			}, {
+		 	 	xtype: 'checkboxfield',
+		 	 	boxLabel: '1990-99',
+		 	 	name: 'years',
+		 	 	inputValue: '199',
+		 	 	checked: true
+		 	}, {
+			 	xtype: 'tbspacer',
+			 	width: 7
+			}, {
+		 	 	xtype: 'checkboxfield',
+		 	 	boxLabel: '2000-09',
+		 	 	name: 'years',
+		 	 	inputValue: '200',
+		 	 	checked: true
+		 	}, {
+			 	xtype: 'tbspacer',
+			 	width: 7
+			}, {
+		 	 	xtype: 'checkboxfield',
+		 	 	boxLabel: '2010-13',
+		 	 	name: 'years',
+		 	 	inputValue: '201',
+		 	 	checked: true
+		 	}, {
+			 	xtype: 'tbspacer',
+			 	width: 20
+			},
+				me.searchButton
+			]
+		}];
 		
 		/**
  		 * @listener
@@ -90,6 +188,12 @@ Ext.define('App.view.d3.geo.basic.MainPanel', {
 			.attr('height', me.canvasHeight);
 		me.svgInitialized = true;
 		
+		// title "g"
+		me.titleG = me.svg.append('svg:g');
+		
+		// circle "g"
+		me.gCircle = me.svg.append('svg:g');
+		
 		// set map properties
 		me.usMap.setSvg(me.svg);
 		me.usMap.setCanvasDimensions(me.canvasWidth, me.canvasHeight);
@@ -108,14 +212,27 @@ Ext.define('App.view.d3.geo.basic.MainPanel', {
 			method: 'GET',
 			success: function(response, options) {
 				var resp = Ext.decode(response.responseText);
+				
 				me.originalGraphData = resp.data;
-				me.graphData = resp.data;
-			},
-			callback: function() {
+				me.graphData = me.filterData(resp.data);
+				
 				me.draw();
 			},
 			scope: me
 		});
+	},
+	
+	filterData: function(dat) {
+		var me = this,
+			ret = [];
+			
+		Ext.each(dat, function(d) {
+			if(me.yearFilter.indexOf(d.year.toString().substr(0, 3)) >=0) {
+				ret.push(d);
+			}
+		}, me);
+		
+		return ret;
 	},
 	
 	draw: function() {
