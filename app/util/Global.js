494785f1069d0aa70d453ef9a2dd1d47001b5cfb
Mark Fehrenbacher <fehrenbacherm@gao.gov>
2014-11-19 14:44:13 -0500

    sortUtils property

--- app/util/Global.js
+++ app/util/Global.js
@@ -13,6 +13,35 @@ Ext.define('App.util.Global', {
 		
 		defaultInfoMessage: 'Demonstrating various visualization libraries in ExtJS.  Heavy on the D3 side.',
 		
+		sortUtils: {
+			dynamicSort: function(property) {
+				return function(obj1, obj2) {
+					return obj1[property] > obj2[property] ? 1 : obj1[property] < obj2[property] ? -1 : 0;
+				}
+			},
+			dynamicMultiSort: function() {
+				/*
+				* save the arguments object as it will be overwritten
+				* note that arguments object is an array-like object
+				* consisting of the names of the properties to sort by
+				*/
+				var props = arguments;
+				return function(obj1, obj2) {
+					var i=0, result=0, numberOfProperties = props.length;
+					
+					/* try getting a different result from 0 (equal)
+					* as long as we have extra properties to compare
+					*/
+					while(result === 0 && i < numberOfProperties) {
+						result = App.util.Global.sortUtils.dynamicSort(props[i])(obj1, obj2);
+						i++;
+					}
+					
+					return result;
+				}
+			}
+		},
+		
 		svg: {
 			currencyTickFormat: function(d) {
 				return Ext.util.Format.currency(d);
